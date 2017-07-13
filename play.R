dc939ac3f0521257eeced485a069c1113b4bfb5f
Stepan Trcka <savage2@email.cz>
2017-07-13 11:17:41 +0200

    doxygen doc

--- play.R
+++ play.R
@@ -1,16 +1,32 @@
+# Useful functions which I've used frequently or which were worth to write down
+# Doxygen doc, sorry for mistakes in english grammar
+
+# @brief Create sequence of random numbers according to the gaussian distribution with default sd and mean
+# @param len length of the sequence
+# @return normalized rnorm sequence
 normalized_rnorm <- function(len) {
 	rand_norm <- rnorm(len)
 	return(rand_norm / max(rand_norm))
 }
 
-# sine-like function with hum
+# @brief Create sine-like fuzzy sequence with specific hum and range
+# @param from interval beginning
+# @param to interval end
+# @param hum_q hum quotient of our "fuzziness"
+# @return Sine fuzzy sequence
 sine_fuzzy_seq <- function(from = 0, to = 4*pi, step = 0.01, hum_q = 1) {
 	t <- seq(from, to, step)
 	seq <- sin(t + hum_q * normalized_rnorm(length(t)))
 	return(seq - min(seq))
 }
 
-# generic function with hum
+# @brief Create generic fuzzy sequence with specific hum and range
+# @param angle_from minimum (ascending/descending) angle between previous and new point and the horizontal axis
+# @param angle_to maximum (ascending/descending) angle between previous and new point and the horizontal axis
+# @param hum_q hum quotient of our "fuzziness"
+# @param num_sections number of sections of our sequence, each section represents one straight line (before fuzzing)
+# @param section_length length of each section
+# @return Generic fuzzy sequence
 generic_fuzzy_seq <- function(angle_from = -pi/2 + 0.1, angle_to = pi/2 + 0.1, hum_q = 5, num_sections = 10, section_length = 20) {
 	# constants
 	possible_angles = runif(100, angle_from, angle_to)
@@ -36,12 +52,11 @@ generic_fuzzy_seq <- function(angle_from = -pi/2 + 0.1, angle_to = pi/2 + 0.1, h
 	return(seq - min(seq))
 }
 
-# ugly linear filter (moving average filter)
+# @brief Linear filter which is based on moving average method	(used formula: 1/(2*a+1) * sum(X_t, from=-a, to=a))
+# @param vec vector (sequence) of numbers to filter
+# @param a interval constant
+# @return filtered sequence
 linear_filter <- function(vec, a) {
-	# my own implementation of linear filter
-	# filter(vec, rep(1/(0.5*a), a))
-	# in other words: 1/(2*a+1) * sum(X_t, from=-a, to=a)
-	
 	values <- double(0) # return values
 	values_index <- 1
 	vec_len <- length(vec)
@@ -59,7 +74,7 @@ linear_filter <- function(vec, a) {
 		}
 		
 		# we cannot use 1/(2*a+1) expression as I've stated before,
-		# because we are operating in range from zero to a-1, not from -a to a (including zero index)
+		# because we operate in range from zero to a-1, not from -a to a (including zero index)
 		values[values_index] = s/a
 		values_index <- values_index + 1
 	}
@@ -67,10 +82,11 @@ linear_filter <- function(vec, a) {
 	return(scale_vec(values, vec_len))
 }
 
-# ugly scale function
-# this function remaps vector (or scale, extend) to new one with different vector length
-# data values are preserved and approximated during the scaling process
-# FYI something similar (but not quite the same) you can achieve with ts() function
+# @brief Ugly function which remaps (or scale) an old vector of values to new one with different length
+# 	Values between points are approximated during the extension process
+# @param vec vector (sequence) of numbers to scale
+# @param new_len new length of your desired vector
+# @return vector after scaling
 scale_vec <- function(vec, new_len) {
 	vec_len <- length(vec)
 	vec_scaled <- double(0)
@@ -85,8 +101,8 @@ scale_vec <- function(vec, new_len) {
 	left_index <- 1
 	
 	# get rid of NA values 
-	# -> if x is NA, then take closest left and right valid value
-	# -> new value for x = (distance_from_left * left_value + distance_from_right * right_value) / (distance_from_left + distance_from_right)
+	# if x is NA, then take closest left and right valid value and determine a new value for x
+	# x = (distance_from_left * left_value + distance_from_right * right_value) / (distance_from_left + distance_from_right)
 	for(i in seq(2, new_len)) {
 		if (!is.na(vec_scaled[i])) {
 			right_val <- vec_scaled[i]
@@ -104,10 +120,20 @@ scale_vec <- function(vec, new_len) {
 	return(vec_scaled)
 }
 
+# @brief Return positive or negative value from given range (remember, given range interval must be positive)
+# @param from lower boundary (from N+)
+# @param to upper boundary (from N+)
+# @return random value
 even_negative_random <- function(from = 0, to = 1) {
 	return (runif(1, from, to) * (if(runif(1) < 0.5) -1 else 1))
 }
 
+# @brief Generate data for linear regression analysis (x and y values)
+# @param num_samples number of data samples
+# @param from X lower limit
+# @param to X upper limit
+# @param var variation between X and Y value
+# @return Data for linear regression 
 gen_data_linear_reg <- function(num_samples = 20, from = 100, to = 200, var = 30) {
 	samples <- c(from:to)
 	x <- sample(samples, num_samples, replace = TRUE)
@@ -120,6 +146,10 @@ gen_data_linear_reg <- function(num_samples = 20, from = 100, to = 200, var = 30
 	return(data.frame(x = x, y = y))
 }
 
+# @brief Get linear function from given sequences of x and y prepared for regression analysis
+# @param x sequence of x values
+# @param y sequence of y values
+# @return Function info (vertical offset and coefficient) for linear regression
 linear_regression <- function(x, y) {
 	avg_x <- mean(x)
 	avg_y <- mean(y)
@@ -128,6 +158,7 @@ linear_regression <- function(x, y) {
 	return(data.frame(b1 = b1, b0 = b0, yoffset = b0, coefficient = b1))
 }
 
+# @brief Generate random data using gen_data_linear_reg(), analyse them using linear_regression() and create a plot
 plot_random_data_lin_reg <- function() {
 	data <- gen_data_linear_reg(num_samples = 50, var = 50)
 	func <- linear_regression(data$x, data$y)
@@ -142,6 +173,10 @@ plot_random_data_lin_reg <- function() {
 	lines(x = c(xmin, xmax), y = c(ymin, ymax))
 }
 
+# @brief My own covariance
+# @param x sequence of x values (same length as y)
+# @param y sequence of y values (same length as x)
+# @return covariance between these values
 my_cov <- function(x, y) {
 	if(length(x) != length(y)) {
 		print("different length x, y")
@@ -163,6 +198,9 @@ my_cov <- function(x, y) {
 	return (sum / (max_index - 1))
 }
 
+# @brief My own variance
+# @param x sequence of x values
+# @return variance between these values
 my_var <- function(x) {
 	avg_x <- mean(x)
 	sum <- 0
